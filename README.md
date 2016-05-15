a68a704c71058327a08bfd6a7d19a805ea3632e8
Ralf S. Engelschall <rse@engelschall.com>
2016-05-15 22:05:03 +0200

    place under version control

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# microkernel-mod-config
-Microkernel module for integrating a YAML configuration file
+
+microkernel-mod-config
+======================
+
+Microkernel module for handling a YAML configuration file.
+
+<p/>
+<img src="https://nodei.co/npm/microkernel-mod-config.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/microkernel-mod-config.png" alt=""/>
+
+About
+-----
+
+This is an extension module for the
+[Microkernel](http://github.com/rse/microkernel) server
+application environment, adding the capability to read
+a YAML configuration file.
+
+Usage
+-----
+
+```shell
+$ npm install microkernel
+$ npm install microkernel-mod-ctx microkernel-mod-options
+$ npm install microkernel-mod-config
+```
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ microkernel-mod-config.js
@@ -0,0 +1,65 @@
+/*
+**  Microkernel -- Microkernel for Server Applications
+**  Copyright (c) 2015-2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import path    from "path"
+import fs      from "fs-promise"
+import co      from "co"
+import YAML    from "js-yaml"
+
+export default class Module {
+    constructor (options) {
+        this.options = Object.assign({
+            configfile: null
+        }, options || {})
+    }
+    get module () {
+        return {
+            name:  "microkernel-config",
+            tag:   "CONFIG",
+            group: "BOOT",
+            after: [ "CTX", "OPTIONS" ]
+        }
+    }
+    latch (kernel) {
+        let configfile = this.options.configfile !== null ? this.options.configfile :
+            path.join(kernel.rs("ctx:basedir"), kernel.rs("ctx:program") + ".yaml")
+        kernel.latch("options:options", (options) => {
+            options.push({
+                name: "config", type: "string", "default": configfile,
+                help: "use YAML file for configuration", helpArg: "FILE" })
+        })
+    }
+    start (kernel) {
+        return co(function * () {
+            let configfile = kernel.rs("options:options").config
+            let exists = yield (fs.exists(configfile))
+            if (!exists)
+                throw new Error(`configuration file not found: ${configfile}`)
+            let yaml = yield (fs.readFile(configfile, "utf8"))
+            let config = YAML.safeLoad(yaml, { filename: configfile })
+            kernel.rs("config", config)
+        }.bind(this))
+    }
+}
+
